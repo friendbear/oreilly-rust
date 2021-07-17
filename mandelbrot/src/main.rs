61bd8730e169c09da354ccb2ef32bcd1b0f7fc12
friendbear <friendbear@users.noreply.github.com>
2021-07-17 16:16:04 +0900

    ピクセルから複素数へのマッピング

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -59,6 +59,28 @@ fn test_parse_complex() {
     assert_eq!(parse_complex(",-0.0625"), None);
 }
 
+// 出力される画像のピクセルの位置を取り、対応する複素平面上の点を返す。
+//
+// `bounds` は出力画像の幅と高さをピクセル単位で与える。
+// `pixel` は画像上の特定のピクセルを(行, 列)ペアの形で指定する。
+// 仮引数 `upper_left`, `lower_right`は、出力画像に描画する複素平面を左上と左下で指定する。
+fn pixel_to_point(bounds: (usize, usize), pixel: (usize, usize),
+    upper_left: Complex<f64>, lower_right: Complex<f64>) -> Complex<f64> {
+    let (width, height) = (lower_right.re -upper_left.re,
+                    upper_left.im - lower_right.im);
+
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+}
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 100), (25, 75),
+        Complex {re: -1.0, im: 1.0},
+        Complex {re: 1.0, im: -1.0}), Complex { re: -0.5, im: -0.5});
+}
+
 fn main() {
     println!("Hello, world!");
 }
