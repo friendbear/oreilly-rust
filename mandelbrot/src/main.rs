c1a1487d10d6594aba034491668a0101d6e73dda
friendbear <friendbear@users.noreply.github.com>
2021-07-17 15:40:14 +0900

    imp parse_complex.

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -43,6 +43,22 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
 
+// Parse apair of floating-point mumbers separated by acomma as a complexnumber.__rust_force_expr!
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex {re, im}),
+        None => None 
+    }
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(parse_complex("1.25,-0.0625"),
+        Some(Complex { re: 1.25, im: -0.0625}));
+
+    assert_eq!(parse_complex(",-0.0625"), None)
+}
+
 fn main() {
     println!("Hello, world!");
 }
